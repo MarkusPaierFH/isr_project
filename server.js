30555d6616eb68961fb2343801fec660818e47ba
Markus Paier <markuspaier@10.0.0.145>
2022-11-03 17:14:03 +0100

    Fixed request and response

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 app.get('/', function (req, res) {
    res.send('Hello World');
@@ -7,17 +8,18 @@ app.get('/', function (req, res) {
 
 
 app.post('/login', function (req, res) {
-   var pass = req.params.password;
-   var email = req.params.email;
-   var name = req.params.name;
+   //var pass = req.params.password;
+   //var email = req.params.email;
+   var name = req.query.name;
 
-   const sqlite3 = require('sqlite3').verbose();
+   console.log(name);
 
-   // open the database
    let db = new sqlite3.Database('./db/nopass.sqlite3');
 
    let sql = `SELECT email, password, name FROM users WHERE name = ?`;
    var names = '';
+   var password = '';
+   var mail = '';
 
    db.all(sql, name, (err, rows) => {
    if (err) {
@@ -25,14 +27,19 @@ app.post('/login', function (req, res) {
    }
    rows.forEach((row) => {
       console.log(row.name);
-      names += row.name;
+      console.log(row.password);
+      console.log(row.email);
+
+      names += row.name + ", ";
+      password += row.password + ", ";
+      mail += row.email + ", ";
    });
    });
 
-   // close the database connection
-   db.close();
-
-   res.send(names);
+   db.close((err) => {
+      if(err){console.log(err.message); return}
+      res.send("name: " + names + " pass: " + password + " email: " + mail);
+  });
 })
 
 var server = app.listen(8081, function () {
